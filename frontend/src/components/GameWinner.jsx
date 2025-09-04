53f653d788d66b5b844ac0d98329f905a1a4c46e
ba9hh <zedtourheart@gmail.com>
2025-09-04 13:15:44 +0100

    Initial commit

--- frontend/src/components/GameWinner.jsx
+++ frontend/src/components/GameWinner.jsx
@@ -9,11 +9,11 @@ const GameWinner = () => {
       <BabarLogo />
       <div className="flex flex-col w-full h-screen justify-center items-center px-10">
         <div className="flex justify-center w-full my-7">
-          <h1 className="text-sm text-gray-400 font-medium border-[1.5px] border-gray-500 px-4 py-1 rounded-[4px]">
+          <h1 className="text-lg text-gray-400 font-medium border-[1.5px] border-gray-500 px-4 py-1 rounded-[4px]">
             The best Spacetoon Song Tournament winner is: {game[0].name}
           </h1>
         </div>
-        <div className="flex items-center w-full justify-between">
+        <div className="flex items-center w-full justify-center">
           <div>
             <div className="w-[640px] h-[360px]">
               <img
