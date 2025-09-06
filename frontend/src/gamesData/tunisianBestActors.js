b85c1a72208050873f0528301d554e4c94a10710
ba9hh <zedtourheart@gmail.com>
2025-09-06 10:55:17 +0100

    Initial commit

---
+++ frontend/src/gamesData/tunisianBestActors.js
@@ -0,0 +1,19 @@
+const tunisianBestActors = [
+    { name: "Dhafer L’Abidine", imageLink: "https://www.webdo.tn/fr/wp-content/uploads/2018/12/Dhafer-CIFF-750x375.jpg" },
+    { name: "Mohamed Mrad", imageLink: "https://polture.com/wp-content/uploads/2023/04/Mohamed-Mrad-1024x609.jpg" },
+    { name: "Fethi Haddaoui", imageLink: "https://cdnfr.africanmanager.com/wp-content/uploads/2024/12/IMGBN60532haddaoui.jpg" },
+    { name: "Kamel Touati", imageLink: "https://pictures.artify.tn/media/o0b099wkrqtrtpb0wlil.jpg" },
+    { name: "Hichem Rostom", imageLink: "https://media.ouest-france.fr/v1/pictures/MjAyMjA2ZWEwZTM3M2FiNGM4MmRmMTRiNzA5NjMyNzYxNzE3MzY?width=1260&height=708&focuspoint=50%2C25&cropresize=1&client_id=bpeditorial&sign=735e20753a0b84379814092a4fff0cba34dfa50d33d44e4715526bfe7cb40f49" },
+    { name: "Yassine Ben Gamra", imageLink: "https://yassinebengamra.com/wp-content/uploads/2022/07/profile-2.jpg" },
+    { name: "Nidhal Saadi", imageLink: "https://m.media-amazon.com/images/M/MV5BM2RiZWE3NGQtZjg0MC00OTExLThlYmMtNjNkOTgxNmRmMDU1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
+    { name: "Mhadheb Rmili", imageLink: "https://content.mosaiquefm.net/uploads/media/1694271259_media.jpg" },
+    { name: "Lotfi Abdelli", imageLink: "https://img.lemde.fr/2022/08/09/0/54/732/488/1440/960/60/0/f25d556_1660059382994-abdelli.jpg" },
+    { name: "Moez Gdiri", imageLink: "https://polture.com/wp-content/uploads/2020/05/moez-gdiri.jpg" },
+    { name: "Faisal Bezzine", imageLink: "https://content.mosaiquefm.net/uploads/media/1730555916.png" },
+    { name: "Mohamed Ali Ben Jemaa", imageLink: "https://i.pinimg.com/736x/29/9e/c3/299ec3131e9916f9747aa532dca41322.jpg" },
+    { name: "Bahri Rahali", imageLink: "https://i.ytimg.com/vi/ui_ikD9IXnE/maxresdefault.jpg" },
+    { name: "Chedly Arfaoui", imageLink: "https://content.mosaiquefm.net/uploads/content/thumbnails/chedly_arfaoui_1680967498.jpg" },
+    { name: "Sofiene Chaari", imageLink: "https://www.babnet.net/cache/cacheimages/06680479d068dc8867fc36c89646ea48_w400.webp" },
+    { name: "Karim Gharbi", imageLink: "https://www.businessnews.com.tn/images/album/IMGBN118601Karim-Gharbi.jpg" },
+]
+export default tunisianBestActors;
---
+++ frontend/src/gamesData/tunisianPretiestActress.js
@@ -0,0 +1,19 @@
+const tunisianPretiestActress = [
+    { name: "Dorra Zarrouk", imageLink: "https://polture.com/wp-content/uploads/2020/10/dorra-zarrouk-el-gouna-film-festival.jpg" },
+    { name: "Souhir Ben Amara", imageLink: "https://www.souhir-benamara.com/EN/images/1-12_02.jpg" },
+    { name: "Leila Ben Khalifa", imageLink: "https://www.nextplz.fr/wp-content/uploads/nextplz/2023/12/leila-ben-khalifa-secret-story-enceinte-elle-annonce-sa-grossesse-quelques-jours-apres-la-naissance-de-lenfant-de-son-ex.jpg" },
+    { name: "Maram Ben Aziza", imageLink: "https://tuniscope.com/uploads/images/content/marambaziza-300416-1-1.jpg" },
+    { name: "Samira Magroun", imageLink: "https://polture.com/wp-content/uploads/2023/10/Samira-Magroun-1024x637.jpg" },
+    { name: "Rim Riahi", imageLink: "https://polture.com/wp-content/uploads/2023/10/Samira-Magroun-1024x637.https://i0.wp.com/magazine.lapresse.tn/wp-content/uploads/2025/03/rimriahi.jpg?fit=1920%2C1280&ssl=1" },
+    { name: "Hend Sabry", imageLink: "https://acc.film/img/blog/8a86ef42a1/3879bd8b7b.jpg" },
+    { name: "Najla Ben Abdallah", imageLink: "https://content.mosaiquefm.net/uploads/content/thumbnails/najla_ben_abdallah_1692829273.jpg" },
+    { name: "Aïcha Ben Ahmed", imageLink: "https://cdn2.webmanagercenter.com/wmc/wp-content/uploads/2024/11/aichabenahmed-ciff.jpg" },
+    { name: "Rym Ben Messaoud", imageLink: "https://cinematunisien.com/wp-content/uploads/2020/02/Rym-Ben-Messaoud-%C2%A9-Mosaique-FM.jpg" },
+    { name: "Yasmine Bouabid", imageLink: "https://content.mosaiquefm.net/uploads/content/large/1743281986.png" },
+    { name: "Azza Slimene", imageLink: "https://www.mojeh.com/wp-content/uploads/2025/05/mojeh1015_a_d832fe06ef.jpg" },
+    { name: "Mariem Ben Hussein", imageLink: "https://cdn2.webmanagercenter.com/di/wp-content/uploads/2019/05/mariem-ben-hussein-carthage-.jpg" },
+    { name: "Marwa Agrebi", imageLink: "https://polture.com/wp-content/uploads/2022/07/Marwa-Agrebi.jpg" },
+    { name: "Ahlem Fekih", imageLink: "https://content.mosaiquefm.net/uploads/content/thumbnails/ahlem_fekih_mes_ambitions_passent_avant_l_amour_1558739136.jpg" },
+    { name: "Myriem Boukadida", imageLink: "https://i.ytimg.com/vi/pl7WB9qjx10/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGDMgVShyMA8=&rs=AOn4CLBECoo2k5gxAC0sJS6LBWS_Cft8oQ" },
+]
+export default tunisianPretiestActress;
